d7f659c52886326be4fa393b668b15772332d481
Surya Thapar <122641977+suryaThapar@users.noreply.github.com>
2024-12-16 16:00:52 +0530

    Update page.tsx

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import BreadcrumbCart from "@/components/cart-page/BreadcrumbCart";
+// import BreadcrumbCart from "@/components/cart-page/BreadcrumbCart";
 import ProductCard from "@/components/cart-page/ProductCard";
 import { Button } from "@/components/ui/button";
 import InputGroup from "@/components/ui/input-group";
